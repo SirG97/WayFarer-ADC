07cdcec9972aa4ca3342e2da6fbf3a4f9c810e55
SirG97 <sirgittarus@gmail.com>
2019-07-12 10:42:13 +0100

    chore: running some db test

--- src/DB/db.js
+++ src/DB/db.js
@@ -1,29 +1,29 @@
-const pg = require('pg');
-// const dotenv = require('dotenv');
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
 
-// dotenv.config();
+dotenv.config();
 
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL
-// });
-
-// pool.on('connect', () => {
-//   console.log('connected to the db');
-// });
-
-const config = {
-  user: 'wayfarer', // this is the db user credential
-  database: 'wayfarer',
-  password: 'rrwcscrz1',
-  port: 5432,
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000
-};
-const pool = new pg.Pool(config);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
 
 pool.on('connect', () => {
-  console.log('connected to the Database');
+  console.log('connected to the db');
 });
+
+// const config = {
+//   user: 'wayfarer', // this is the db user credential
+//   database: 'wayfarer',
+//   password: 'rrwcscrz1',
+//   port: 5432,
+//   max: 10, // max number of clients in the pool
+//   idleTimeoutMillis: 30000
+// };
+// const pool = new pg.Pool(config);
+
+// pool.on('connect', () => {
+//   console.log('connected to the Database');
+// });
 
 /**
  * Create Tables
